46702be13bb32f6c62fbd782264f4ac61fe169e1
Luthfi Alfaridz <luthfifahmi.alv@gmail.com>
2023-09-21 19:37:08 +0700

    menambahkan file biodata Group E

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("checkGuess").addEventListener("click", function () {
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    const guessedNumber = parseInt(document.getElementById("guess").value);
+    const gameResultElement = document.getElementById("gameResult");
+
+    if (guessedNumber === randomNumber) {
+        gameResultElement.innerText = "Wihh,, bener cuyy  " + randomNumber;
+        gameResultElement.style.color = "green";
+    } else {
+        gameResultElement.innerText = "Yaahh,, salah :)" + randomNumber;
+        gameResultElement.style.color = "red";
+    }
+
+    // document.getElementById("guess").disabled = true;
+    // document.getElementById("checkGuess").disabled = true;
+});
